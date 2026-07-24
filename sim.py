e297dbf1cac46b7b39330cc6d3706ce6b6c5ce21
Zachary Jacobson <zach.h.jacobson@gmail.com>
2026-07-24 18:01:09 -0500

    Simulate heart grind
    Simulate a full run of Mortimer until an imbued heart drops, weighing each task and unlocking more options as tasks are completed, tallying up the time taken.

--- sim.py
+++ sim.py
@@ -5,9 +5,9 @@ import random
 TASK_PREP_TIME = 30 # time to go back to Mortimer, choose task, restore stats if desired, and bank for new task
 HOUR = 6000
 SUPERIOR_RATE = 150
-SIMS_PER_TASK = 10000
-HEARTS_SIMULATED = 10000
-TIME_PER_HEART = 70 * HOUR
+SIMS_PER_TASK = 5000
+HEARTS_SIMULATED = 10
+TIME_PER_HEART = 600 * HOUR
 class Bracelet(Enum):
     NONE = 1
     SLAUGHTER = 2
@@ -17,17 +17,34 @@ with open("Mortimer.csv", mode="r") as file:
     tasks = list(csv.reader(file))[1:]
 
 total_weight = 0
-for task in tasks:
-    total_weight += int(task[2])
-
-superiors_count = 0
-simming_task = False
+for t in tasks:
+    total_weight += int(t[2])
 
 def main():
-    task_options = choose_task_options("Hydras", 100)
-    for option in task_options:
-        ticks_wasted = sim_ticks_wasted(get_task(option[0]), int(option[1]), int(option[2]))
-        print(f"{option[0]} task with {option[1]} quantity and {option[2]}% more hearts wastes {ticks_wasted/HOUR} hours")
+    got_heart = False
+    total_time = 0
+    last_task = "None"
+    tasks_completed = 0
+    while(got_heart == False):
+        task_options = choose_task_options(last_task, tasks_completed)
+        task_ratings = []
+        for option in task_options:
+            ticks_wasted = sim_ticks_wasted(get_task(option[0]), int(option[1]), int(option[2]))
+            task_ratings.append(ticks_wasted)
+        best_option = task_ratings.index(min(task_ratings))
+        last_task = task_options[best_option][0]
+        best_task = get_task(last_task)
+        if(task_ratings[best_option] < 0):
+            task_length = calc_task_length(best_task, int(task_options[best_option][1]), Bracelet.SLAUGHTER, False)
+            print(f"doing {best_task[1]} with {option[1]} quantity and {option[2]}% more hearts using slaughter bracelets over {task_options}")
+        else:
+            task_length = calc_task_length(best_task, int(task_options[best_option][1]), Bracelet.EXPEDITIOUS, False)
+            print(f"doing {best_task[1]} with {option[1]} quantity and {option[2]}% more hearts using expeditious bracelets over {task_options}")
+        total_time += calc_time(best_task, task_length)
+        got_heart = check_for_heart(best_task, task_length, int(task_options[best_option][2]))
+        tasks_completed += 1
+    print(f"total time: {total_time}, tasks completed: {tasks_completed}")
+    
 
 def choose_task_options(last_task: str, tasks_completed: int):
     task_options: list[list[str]]
@@ -73,8 +90,8 @@ def choose_task_options(last_task: str, tasks_completed: int):
                 task_options.append([chosen_task, "0", "0"])
             #superior drop rate
             case 3:
-                length_modifier = random.randrange(int(task[8]), int(task[9]) + 1)
-                task_options.append([chosen_task, "0", "30"])
+                superior_modifier = random.randrange(int(task[8]), int(task[9]) + 1)
+                task_options.append([chosen_task, "0", superior_modifier])
             #slayer xp
             case 4:
                 task_options.append([chosen_task, "0", "0"])
@@ -82,25 +99,23 @@ def choose_task_options(last_task: str, tasks_completed: int):
             
 
 def sim_ticks_wasted(task: list[str], length_modifier: int, drop_modifier: int):
-    global simming_task
-    simming_task = True
-
-    task_completion_time = TASK_PREP_TIME + int(task[17])
-    task_completion_time += calc_task_length(task, length_modifier, Bracelet.SLAUGHTER, True)
+    task_completion_time = calc_time(task, calc_task_length(task, length_modifier, Bracelet.SLAUGHTER, True))
     total_time = 0
     for sim in range(SIMS_PER_TASK):
         task_length = calc_task_length(task, length_modifier, Bracelet.SLAUGHTER, False)
         got_heart = False
         while(got_heart == False):
-            total_time += TASK_PREP_TIME
-            total_time += int(task[17])
-            total_time += int((float(task_length) / int(task[18])) * HOUR)
+            total_time += calc_time(task, task_length)
             got_heart = check_for_heart(task, task_length, drop_modifier)
 
     task_time_per_heart = total_time / SIMS_PER_TASK
 
-    simming_task = False
     return(task_completion_time * (1-(TIME_PER_HEART/task_time_per_heart)))
+
+def calc_time(task: list[str], task_length: int):
+    time = TASK_PREP_TIME + int(task[17])
+    time += int(task_length / float(task[18])) * HOUR
+    return(time)
 
 
 def get_task(task_name: str):
@@ -133,16 +148,8 @@ def check_for_heart(task: list[str], task_length: int, drop_modifier: int):
     return False
 
 def spawn_superior(heart_chance: int, drop_modifier: int):
-    increment_superiors()
     heart_chance = int((heart_chance * 100) / (100 + drop_modifier))
     return(random.randrange(heart_chance) == 0)
-
-def increment_superiors():
-    global simming_task
-    if(not simming_task):
-       global superiors_count
-       superiors_count += 1
-    
 
 if __name__ == '__main__':
     main()
